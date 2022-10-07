7505f20f91f7a13f92fadbbec5401d00f63d2534
lqnoei <114935497+lqnoei@users.noreply.github.com>
2022-10-07 11:57:51 +0200

    Update main.c

--- src/main.c
+++ src/main.c
@@ -1,29 +1,19 @@
 #include <stdio.h>
 #include <uncertain.h>
 
-void readMatrix(int n, double M[n][n+1]){
-    int i,j;
-for(i=0;i<n;i++){
-    for(j=0;j<(n+1);j++){
-        printf("M[%d][%d]:", i,j);
-        scanf("%d",&M[i][j]);
-    }
-  }
-}
-
 void printMatrix(int n, double M[n][n+1]){
     int i,j;
     for(i=0;i<n;i++){
         for(j=0;j<n+1;j++){
-            printf("%lf\t",matrix[i][j]);
+            printf("%lf\t",M[i][j]);
         }
         printf("\n");
     }
 }
 
-void double GE_upper(int n, double M[n][n+1], double x[n]){
+void  GE_upper(int n, double M[n][n+1], float x[n]){
     
-    int i,j,k;
+    int i,j,k,C,V;
     
     /*the multiplier*/
     double s;
@@ -32,74 +22,74 @@ void double GE_upper(int n, double M[n][n+1], double x[n]){
      elimitation(upper triangular matrix)
      */
     for (i=0;i<n;i++){
-        for(j=1; j<n;j++ ){
+        for(j=1+i ; j<n;j++ ){
             s=M[j][i]/M[i][i];
-            for (k=0; k<n+1;k++ )
-            {M[j][k]=M[j][k]-s*M[i][k]
-                
+            for (k=0; k<=n;k++ ){
+                M[j][k]=M[j][k]-s*M[i][k];
             }
         }
     }
     /*the result of solving the trangular matrix x[n-1] might be zero
      */
-    double  sum=0.0;
-    
-    x[n-1]=M[n-1][n]/M[n-1][n-1];
-    for (i=n-2;i>=1;i--){
-        sum=x[i+1]*M[i][i+1]+sum;
-        x[i]= (M[i][n]-sum)/M[i][i]
-    }
-    
-}
-
-
-int
-main(int argc, char *  argv[])
-{
-	double	a, b, c;
-
-	a = libUncertainDoubleUniformDist(0.5, 1.0);
-	printf("a = %lf\n", a);
-
-	b = libUncertainDoubleUniformDist(10.0, 20.0);
-	printf("b = %lf\n", b);
-
-	c = (a+b)/(a-b);
-	printf("c = %lf\n", c);
-	
-
-	    /* get the matrix*/
-    int n;
-    printf("the dimension of matrix M:");
-    scanf("%d",&n );
-    
-    double M1[n][n+1],M2[n][n+1],x[n];
-    
-    printf("\nEnter the elements of augmented matrix row-wise:\n\n");
-    readMatrix(n,M1[n][n+1])
-    int i,j;
-        for(i=0;i<n;i++){
-            for(j=0;j<(n+1);j++){
-                M2[i][j]=M1[i][j];
-            }
+    x[n-1]=(M[n-1][n])/(M[n-1][n-1]);
+    for (C=n-2;C>=0;C--){
+        x[C]=M[C][n];
+        for(V=C+1;V<n;V++){
+            x[C]=x[C]-(x[V]*M[C][V]);
         }
+        x[C]= x[C]/(M[C][C]);
     }
-printf("\nThe original matrix is:\n\n");
-printMatrix(n,M2);
-GE_upper(n,M1,x);
-printf("\nThe upper Triangular matrix after Gaussian Eliminiation is:\n\n");
-printMatrix(n.M1);
     
-    
-    printf("The solution is:\n")
-    for (i=0;i<n;i++){
-        printf("\nx[%d]=%d\t",i+1,x[i])
-    }
-
-#ifdef DEBUG
-	printf("debug message\n");
-#endif
-
-	return 0;
 }
+
+
+    
+    int
+    main(int argc, char *  argv[])
+{
+        /* get the dimension*/
+        int n;
+        n=2;
+
+        /*construct a matrix where entries are distributed variables */
+        
+        double M1[n][n+1];
+        M1[0][0]=libUncertainDoubleExponentialDist(10.5);
+        M1[0][1]=libUncertainDoubleExponentialDist(10.5);
+        M1[1][0]=libUncertainDoubleExponentialDist(10.5);
+        M1[1][1]=libUncertainDoubleExponentialDist(20.5);
+        M1[0][2]=libUncertainDoubleExponentialDist(20.5);
+        M1[1][2]=libUncertainDoubleExponentialDist(30.5);
+        /*View the original matrix*/
+        printf("The original matrix is:\n");
+        printMatrix(n, M1);
+        
+        /* do the gaussian elimitation*/
+        
+        float x[n];
+        
+        GE_upper(n, M1,x);
+        printf("The upper triangular matrix is:\n");
+        printMatrix(n, M1);
+        printf("The printed solution to the equations is:\n");
+        int r;
+        for (r=0;r<n;r++){
+            printf("x[%d]=%lf\t", r, x[r]);
+        }
+
+
+        printf("\nThe expected solution to the equations is:\n");
+        double x_E[n];
+        int i;
+        for (i=0;i<n;i++){
+            x_E[i]=libUncertainDoubleNthMoment(x[i],1);
+            printf("x[%d]=%lf\t", i, x_E[i]);
+        }
+    
+#ifdef DEBUG
+        printf("debug message\n");
+#endif
+        
+        return 0;
+    }
 
